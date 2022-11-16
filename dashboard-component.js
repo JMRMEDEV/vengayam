cb042d39fa795ad77d4b0006acd3dd2366327030
Josue Martinez Rodriguez <josue.martinezrzmx@gmail.com>
2022-11-15 23:37:40 -0600

    First commit

---
+++ dashboard-component.js
@@ -0,0 +1,104 @@
+/**
+ * Function that appends other scripts to current one, allowing us to isolate
+ * each component in separate files but using them as needed.
+ * @param {*} url
+ */
+const loadScript = (url) => {
+  var head = document.getElementsByTagName("head")[0];
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = url;
+  head.appendChild(script);
+};
+
+const colors = {
+  svbBlue: {
+    darker: "#10466a",
+    dark: "#1164a8",
+    medium: "#2b70b6",
+    mediumLighter: "#3f80bd",
+    light: "#6fc0ec",
+  },
+  gray: {
+    light: "#fafafa",
+    darker: "#e0e0e0",
+  },
+};
+
+class DashboardComponent extends HTMLElement {
+  constructor() {
+    super();
+    const template = document.createElement("template");
+    template.innerHTML = this.getInnerHTML();
+    // Load widgets' scripts
+    loadScript("widgets/widget-wrapper.js");
+    loadScript("widgets/cash-in-cash-out.js");
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  /**
+   * Function meant to isolate the HTML template from the constructor, for
+   * terms of order and structure.
+   * @returns
+   */
+  getInnerHTML() {
+    return /*html*/ `
+    <style>
+      .dashboard-section:hover {
+        font-weight: bold;
+        cursor: pointer;
+        background-color: ${colors.svbBlue.mediumLighter};
+      }
+      .column-container {
+        
+      }
+    </style>
+    <!-- Dashboard main container -->
+    <div className='dashboard-widget' style="display: flex; flex-direction: row; min-width: 100vh; min-height: 100vh; margin: -8px;">
+      <!-- Left panel -->
+      <div style="display: flex; flex-direction: column; min-height: 100vh; width: 10%; background-color: ${
+        colors.svbBlue.medium
+      };">
+        <div class="dashboard-section" style="color: white; display: flex; justify-content: center; align-items: center; height: 70px; width: 100%; border-style: none none none solid; border-width: 2px; border-color: white; margin-top: 20px;">
+          Dashboard
+        </div>
+      </div>
+      <!-- Rest of the content that is not the panel -->
+      <div id="content-container" style="display: flex; flex-direction: column; min-height: 100vh; width: 90%;">
+        <!-- Navbar -->
+        <div style="width: 100%; min-height: 10%; background-color: ${
+          colors.svbBlue.darker
+        };">
+          <div style="padding: 20px 0px 20px 20px;">
+            <h1 style="color: white; font-size: 16px;">Welcome to Vengayam</h1>
+            <p style="color: white;">
+              ${new Date().toLocaleString("en-US", {
+                timeZone: "PST",
+              })}
+            </p>
+          </div>
+        </div>
+        <!-- Columns -->
+        <div style="display: flex; flex-direction: row; height: 90%; align-items: flex-start;">    
+          <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; width: 50%;">
+            <slot name="column-0">Column 0 children</slot>
+          </div>
+          <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; width: 50%;">
+            <slot name="column-1">Column 1 children</slot>
+          </div>
+        </div>
+      </div> 
+    </div>`;
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.getElementsByClassName("dashboard-component");
+  }
+}
+
+window.customElements.define("dashboard-component", DashboardComponent);
